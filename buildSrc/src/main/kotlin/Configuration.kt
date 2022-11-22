0dec80bb1b8a74e0d386f7ec65b16014a3d08f79
djy <137359049@qq.com>
2022-11-22 11:24:21 +0800

    1.lunar = "1.2.26" 2.kotlin = "1.7.10"

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,21 +1,21 @@
 @file:Suppress("unused", "SpellCheckingInspection")
 
 object Android {
-    const val compileSdk = 31
+    const val compileSdk = 32
     const val minSdk = 21
-    const val targetSdk = 30
+    const val targetSdk = 32
 
-    const val versionName = "1.0"
-    const val versionCode = 1
+    const val versionName = "1.0.0"
+    const val versionCode = 10000
 }
 
 object Versions {
-    const val kotlin = "1.6.21"
-    const val lunar = "1.2.22"
+    const val kotlin = "1.7.10"
+    const val lunar = "1.2.26"
 }
 
 object Deps {
-    const val appcompat = "androidx.appcompat:appcompat:1.4.1"
-    const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.1.3"
+    const val appcompat = "androidx.appcompat:appcompat:1.5.1"
+    const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.1.4"
     const val lunar = "cn.6tail:lunar:${Versions.lunar}"
 }
